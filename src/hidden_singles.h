c170d3554fe2dae0ef1f203a5b82f0df71ff39fc
ar1st0n <134787767+ar1st0n@users.noreply.github.com>
2023-12-31 23:13:09 +0700

    Add files via upload

--- src/hidden_singles.h
+++ src/hidden_singles.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "sudoku.h"
 
 typedef struct HiddenSingle_impl
@@ -8,7 +7,11 @@ typedef struct HiddenSingle_impl
     int value;
 } HiddenSingle;
 
+// find hidden single values (the value that is unique among all the candidates for a row, a collumn, or a box)
 int find_hidden_single_values(Cell **p_cells, int *hidden_single_values);
-void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles,
-                        int *p_counter);
-int hidden_singles(SudokuBoard *p_board, Cell ***p_solved_cells);
+
+// find hidden single cells in a row, in a collumn or in a box
+void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter);
+
+// find hidden single cells in a board, return the number of hidden single cells
+int hidden_singles(SudokuBoard *p_board);
--- src/sudoku.h
+++ src/sudoku.h
@@ -1,29 +1,34 @@
+// sudoku.h
 #pragma once
-
+#include <assert.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define BOARD_SIZE 9
 
-struct Cell_impl {
+struct Cell_impl
+{
     int row_index;
     int col_index;
     int box_index;
     int num_candidates;
     int candidates[BOARD_SIZE];
-    int value;  // solved or 0
+    int value; // solved or 0
+    bool fixed;
 };
 
 typedef struct Cell_impl Cell;
 
-struct SudokuBoard_impl {
+struct SudokuBoard_impl
+{
     int solved_counter;
-    Cell **data;  // 9x9 cell board
+    Cell **data; // 9x9 cell board
 
-    Cell **p_rows[BOARD_SIZE];   // rows pointers
-    Cell **p_cols[BOARD_SIZE];   // cols pointers
-    Cell **p_boxes[BOARD_SIZE];  // boxes pointers
-    Cell *solved_cells[BOARD_SIZE *
-                       BOARD_SIZE];  // solved cell pointers (maximum)
+    Cell **p_rows[BOARD_SIZE];                   // rows pointers
+    Cell **p_cols[BOARD_SIZE];                   // cols pointers
+    Cell **p_boxes[BOARD_SIZE];                  // boxes pointers
+    Cell *solved_cells[BOARD_SIZE * BOARD_SIZE]; // solved cell pointers (maximum)
 };
 
 typedef struct SudokuBoard_impl SudokuBoard;
@@ -35,7 +40,12 @@ bool is_in_list(Cell **p_array, int size, Cell *p);
 void print_candidate_num(SudokuBoard *p_board);
 void print_solution(SudokuBoard *p_board);
 
+bool is_candidate(Cell *cell, int value);
 void set_candidate(Cell *cell, int value);
 void unset_candidate(Cell *cell, int value);
 void set_candidates(Cell *cell, int *candidates, int size);
 int *get_candidates(Cell *cell);
+
+int check_solved_cells(SudokuBoard *p_board, Cell ***p_solved_cells);
+bool show_possible(SudokuBoard *p_board, Cell **p_solved_cells, int counter);
+void free_sudoku(SudokuBoard *p_board);
